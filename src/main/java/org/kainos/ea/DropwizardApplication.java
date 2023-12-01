bf63a6a2be926eb8b059ef8627dec8001d10842f
CharlesM988 <charles.mullan@kainos.com>
2023-12-01 15:44:24 +0000

    restructuring the api application

---
+++ src/main/java/org/kainos/ea/DropwizardApplication.java
@@ -0,0 +1,35 @@
+package org.kainos.ea;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+import io.federecio.dropwizard.swagger.SwaggerBundle;
+import io.federecio.dropwizard.swagger.SwaggerBundleConfiguration;
+
+public class DropwizardApplication extends Application<DropwizardConfiguration> {
+
+    public static void main(final String[] args) throws Exception {
+        new DropwizardApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "ih-java-api";
+    }
+
+    @Override
+    public void initialize(final Bootstrap<DropwizardConfiguration> bootstrap) {
+        bootstrap.addBundle(new SwaggerBundle<DropwizardConfiguration>(){
+            @Override
+            protected SwaggerBundleConfiguration getSwaggerBundleConfiguration(DropwizardConfiguration configuration) {
+                return configuration.getSwagger();
+            }
+        });    }
+
+    @Override
+    public void run(final DropwizardConfiguration configuration,
+                    final Environment environment) {
+        // TODO: implement application
+    }
+
+}
---
+++ src/main/java/org/kainos/ea/db/DatabaseConnector.java
@@ -0,0 +1,44 @@
+package org.kainos.ea.db;
+
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DatabaseConnector {
+    private static Connection connection;
+
+    public static Connection getConnection() throws SQLException {
+        String user, password, host, name;
+
+        if (connection != null && !connection.isClosed()) {
+            return connection;
+        }
+
+        try (FileInputStream propsStream = new FileInputStream("db.properties")) {
+
+            Properties props = new Properties();
+            props.load(propsStream);
+
+            user = props.getProperty("user");
+            password = props.getProperty("password");
+            host = props.getProperty("host");
+            name = props.getProperty("name");
+
+            if (user == null || password == null || host == null) {
+                throw new IllegalArgumentException("Properties file must exist " +
+                        "and must contain user, password, name, and host properties");
+            }
+
+            connection = DriverManager.getConnection("jdbc:mysql://" +
+                    host + "/" + name +
+                    "?useSSL=false", user, password);
+            return connection;
+        }
+        catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+        return null;
+    }
+}
