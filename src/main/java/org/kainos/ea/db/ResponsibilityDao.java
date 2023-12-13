f5f3bb89977d96ded4f1003b16be5147891ca271
CharlesM988 <charles.mullan@kainos.com>
2023-12-13 15:50:10 +0000

    initial commit of adding role respons to view-job-spec.html - API

---
+++ src/main/java/org/kainos/ea/db/ResponsibilityDao.java
@@ -0,0 +1,44 @@
+package org.kainos.ea.db;
+
+import org.kainos.ea.cli.Responsibility;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ResponsibilityDao {
+
+    private static Connection conn;
+
+    private DatabaseConnector databaseConnector = new DatabaseConnector();
+
+    public List<Responsibility> getRoleResponsibilityById(int id) throws SQLException {
+        String query = "SELECT rt.responsibilityID, rt.responsibilityName FROM Responsibility rt JOIN JobRole jr ON rt.roleID = jr.roleID WHERE jr.roleID = ?";
+
+        List<Responsibility> responsibilitiesList = new ArrayList<>();
+
+        try (Connection c = databaseConnector.getConnection();
+             PreparedStatement ps = c.prepareStatement(query)) {
+
+            ps.setInt(1, id);
+
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    Responsibility responsibility = new Responsibility();
+                    responsibility.setResponsibilityName(rs.getString("responsibilityName"));
+                    responsibilitiesList.add(responsibility);
+                }
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Error executing SQL query", e);
+        }
+
+        return responsibilitiesList;
+    }
+
+
+}
