1e7a5a001f851a9f1f07dba76c564455af77a964
Caitlin Bulloch <caitlin.bullloch@kainos.com>
2023-12-14 12:11:26 +0000

    linter fixes 4

--- src/main/java/org/kainos/ea/cli/Job.java
+++ src/main/java/org/kainos/ea/cli/Job.java
@@ -1,37 +1,37 @@
 package org.kainos.ea.cli;
 
-public class Job{
+public class Job {
     private int roleID;
     private String roleName;
     private String capabilityName;
 
-    public Job (int roleID, String roleName, String capabilityName){
+    public Job ( int roleID, String roleName, String capabilityName) {
         this.roleID = roleID;
         this.roleName = roleName;
         this.capabilityName = capabilityName;
     }
 
-    public int getRoleID(){
+    public int getRoleID() {
         return roleID;
     }
 
-    public void setRoleID(int roleID){
+    public void setRoleID(int roleID) {
         this.roleID = roleID;
     }
 
-    public String getRoleName(){
+    public String getRoleName() {
         return roleName;
     }
 
-    public void setRoleName(String roleName){
+    public void setRoleName(String roleName) {
         this.roleName = roleName;
     }
 
-    public String getCapabilityName(){
+    public String getCapabilityName() {
         return capabilityName;
     }
 
-    public void setCapabilityName(String capabilityName){
+    public void setCapabilityName(String capabilityName) {
         this.capabilityName = capabilityName;
     }
 }
