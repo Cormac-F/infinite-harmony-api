f54000ed6ca3561dcedc9b37b356d2e254ae6142
CharlesM988 <145450764+CharlesM988@users.noreply.github.com>
2023-12-14 10:44:03 +0000

    Update JobController.java to remove redundant lines

--- src/main/java/org/kainos/ea/resources/JobController.java
+++ src/main/java/org/kainos/ea/resources/JobController.java
@@ -8,10 +8,6 @@ import org.kainos.ea.client.JobSpecDoesNotExistException;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import org.kainos.ea.client.FailedToGetAllJobsException;
-
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
