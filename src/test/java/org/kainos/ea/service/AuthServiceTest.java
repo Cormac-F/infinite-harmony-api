afde699dcd48fdcd70bce62e250a4fbddfbe6adc
amchugh689 <aideen.mchugh@kainos.com>
2023-12-20 14:34:22 +0000

    added imports

--- src/test/java/org/kainos/ea/service/AuthServiceTest.java
+++ src/test/java/org/kainos/ea/service/AuthServiceTest.java
@@ -1,4 +1,31 @@
 package org.kainos.ea.service;
 
+import org.kainos.ea.cli.Login;
+import org.kainos.ea.db.AuthDao;
+import org.kainos.ea.api.AuthService;
+import org.kainos.ea.client.FailedToLoginException;
+import org.kainos.ea.client.FailedToGenerateTokenException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.kainos.ea.db.DatabaseConnector;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
 public class AuthServiceTest {
+
 }
