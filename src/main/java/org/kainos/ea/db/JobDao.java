cfad81b78440996df0c1d4f48ce4f2eb842b05ed
CharlesM988 <charles.mullan@kainos.com>
2023-12-20 14:43:38 +0000

    fixing liniting problem on jobdao

--- src/main/java/org/kainos/ea/db/JobDao.java
+++ src/main/java/org/kainos/ea/db/JobDao.java
@@ -2,7 +2,11 @@ package org.kainos.ea.db;
 
 import org.kainos.ea.cli.Job;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
