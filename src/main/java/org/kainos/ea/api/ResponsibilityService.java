c58985ee20e0fed100dd8ded037e2de7b492a382
CharlesM988 <charles.mullan@kainos.com>
2023-12-20 10:46:45 +0000

    adding unit tests and fixing various issues

---
+++ src/main/java/org/kainos/ea/api/ResponsibilityService.java
@@ -0,0 +1,32 @@
+package org.kainos.ea.api;
+
+import org.kainos.ea.cli.Responsibility;
+import org.kainos.ea.client.FailedToGetRoleResponsibilityException;
+import org.kainos.ea.client.RoleResponsibilityDoesNotExistException;
+import org.kainos.ea.db.ResponsibilityDao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class ResponsibilityService {
+
+    private ResponsibilityDao responsibilityDao = new ResponsibilityDao();
+
+    public List<Responsibility> getRoleResponsibilityById(int id) throws FailedToGetRoleResponsibilityException,
+            RoleResponsibilityDoesNotExistException {
+        List<Responsibility> responsibilitiesList = null;
+        try {
+            responsibilitiesList = responsibilityDao.getRoleResponsibilityById(id);
+
+            if (responsibilitiesList == null) {
+                throw new RoleResponsibilityDoesNotExistException();
+            }
+
+            return responsibilitiesList;
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+
+            throw new FailedToGetRoleResponsibilityException();
+        }
+    }
+}
