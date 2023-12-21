1e105b35f51a16b366971d508e8e7c85af882191
Caitlin Bulloch <caitlin.bullloch@kainos.com>
2023-12-21 13:47:31 +0000

    fix2

--- src/main/java/org/kainos/ea/client/CapabilityValidator.java
+++ src/main/java/org/kainos/ea/client/CapabilityValidator.java
@@ -3,9 +3,9 @@ package org.kainos.ea.client;
 import org.kainos.ea.cli.CapabilityRequest;
 
 public class CapabilityValidator {
-    private final int MaxLength = 100;
+    private final int maxLength = 100;
     public boolean isValidCapabiility(CapabilityRequest capability) {
-        if (capability.getCapabilityName().length() > MaxLength) {
+        if (capability.getCapabilityName().length() > maxLength) {
             return false;
         }
         return true;
